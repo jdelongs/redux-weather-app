9d1dde4c4ad0026054bf9b9d40499bce37e1275c
jdelong <jdelong15@hotmail.ca>
2018-11-15 04:49:03 -0800

    remove .vscode file

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios'; 
 
 //global variables
-const API_KEY = ''; 
+const API_KEY = 'bd3699d88dada6a54fb5e6bd6e11dcbf'; 
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`; 
 
 //exports 
@@ -11,9 +11,8 @@ export function fetchWeather(city) {
 
     //api request url
     const url = `${ROOT_URL}&q=${city}, us`;
-    //ajax request using axios
+    //ajax request using axios(returns a promise)
     const request = axios.get(url);
-
     return {
         type: FETCH_WEATHER, 
         playload: request
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 
+//reducer imports
+import WeatherReducer from './reducer_weather'; 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
