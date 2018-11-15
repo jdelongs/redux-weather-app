a823878835d07bba3c274be85994d7c34ffd307a
jdelong <jdelong15@hotmail.ca>
2018-11-15 04:32:12 -0800

    added the ability to make api requests using search bar

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,17 @@
+//imports
 import axios from 'axios'; 
 
+//global variables
 const API_KEY = ''; 
-const ROOT_URL = `https://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`; 
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`; 
 
+//exports 
 export const FETCH_WEATHER = 'FETCH_WEATHER'; 
 export function fetchWeather(city) {
+
+    //api request url
     const url = `${ROOT_URL}&q=${city}, us`;
+    //ajax request using axios
     const request = axios.get(url);
 
     return {
